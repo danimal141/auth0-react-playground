aecee654152664af3caf4ee1bf84b6f1b33fc835
danimal141 <hideaki.ishii1204@gmail.com>
2021-05-09 11:07:24 +0900

    Fix auth

--- backend/app/controllers/concerns/authenticatable.rb
+++ backend/app/controllers/concerns/authenticatable.rb
@@ -3,11 +3,9 @@
 module Authenticatable
   extend ActiveSupport::Concern
 
-  SCOPES = {}.freeze
-
-  # included do
-  #   before_action :authenticate_request!
-  # end
+  SCOPES = {
+    '/graphql' => ['openid'],
+  }
 
   def current_user
     @current_user ||= User.from_token_payload(auth_payload)
@@ -17,20 +15,11 @@ module Authenticatable
 
   def auth_payload
     auth_payload, = retrieve_auth_token
-    p '---------'
-    pp auth_payload
+    return unless scope_included?(auth_payload)
     auth_payload
   rescue JWT::VerificationError, JWT::DecodeError, ActiveRecord::RecordInvalid
     nil
   end
-
-  # def authenticate_request
-  #   @auth_payload, @auth_header = auth_token
-  #   @user = User.from_token_payload(@auth_payload)
-  #   render json: { errors: ['Insufficient scope'] }, status: :forbidden unless scope_included
-  # rescue JWT::VerificationError, JWT::DecodeError, ActiveRecord::RecordInvalid
-  #   render json: { errors: ['Not Authenticated'] }, status: :unauthorized
-  # end
 
   def http_token
     request.headers['Authorization'].split(' ').last if request.headers['Authorization'].present?
@@ -40,11 +29,9 @@ module Authenticatable
     JsonWebToken.verify(http_token)
   end
 
-  def scope_included
-    # The intersection of the scopes included in the given JWT and the ones in the SCOPES hash needed to access
-    # the PATH_INFO, should contain at least one element
+  def scope_included?(payload)
     return true if SCOPES[request.env['PATH_INFO']].nil?
 
-    (String(@auth_payload['scope']).split(' ') & (SCOPES[request.env['PATH_INFO']])).any?
+    (String(payload['scope']).split(' ') & (SCOPES[request.env['PATH_INFO']])).any?
   end
 end
